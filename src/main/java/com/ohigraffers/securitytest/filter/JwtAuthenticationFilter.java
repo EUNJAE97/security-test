e8565e9138c571beb1296bb8e2fc3ee7234c7c37
EUNJAE97 <154950170+EUNJAE97@users.noreply.github.com>
2024-05-22 00:38:38 +0900

    Update JwtAuthenticationFilter.java

--- src/main/java/com/ohigraffers/securitytest/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/ohigraffers/securitytest/filter/JwtAuthenticationFilter.java
@@ -1,5 +1,4 @@
 package com.ohigraffers.securitytest.filter;
-
 import com.ohigraffers.securitytest.service.AuthService;
 import com.ohigraffers.securitytest.util.TokenUtils;
 import jakarta.servlet.FilterChain;
@@ -7,13 +6,10 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -23,28 +19,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        String refreshToken = TokenUtils.getToken(request.getHeader("Refresh-Token"));
-        if (refreshToken != null && TokenUtils.isValidToken(refreshToken)) {
-            String username = TokenUtils.getUsername(refreshToken);
-
-            UserDetails userDetails = authService.loadUserByUsername(username);
-            Map<String, Object> memberInfo = new HashMap<>();
-            memberInfo.put("username", userDetails.getUsername());
-            memberInfo.put("password",userDetails.getPassword());
-
-            String newAccessToken = TokenUtils.createAccessToken(memberInfo);
-            response.setHeader("Access-Token", newAccessToken);
-            response.setHeader("Refresh-Token", refreshToken);
-            authService.saveAuthentication(username);
-
-
-        }
-        String accessToken = TokenUtils.getToken(request.getHeader("Access-Token"));
+        /* 알맞은 내용 작성 */
+        String accessToken = TokenUtils.getToken(request.getHeader("Authorization"));
         if(accessToken != null && TokenUtils.isValidToken(accessToken)) {
             String username = TokenUtils.getUsername(accessToken);
             authService.saveAuthentication(username);
         }
-
         filterChain.doFilter(request, response);
 
     }
